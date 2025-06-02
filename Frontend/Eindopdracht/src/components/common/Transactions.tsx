509c82a20f1327186647dac8d976160336dae000
YDevving <78418986+ydevving@users.noreply.github.com>
2025-06-02 15:43:23 +0200

    Resolved conflict merge on Transactions.tsx

--- Frontend/Eindopdracht/src/components/common/Transactions.tsx
+++ Frontend/Eindopdracht/src/components/common/Transactions.tsx
@@ -1,13 +1,4 @@
-import NavbarComponent from "./Navbar"
-import { Outlet } from 'react-router'
 
-export default function Transactions() {
-    return (
-        <>
-           <h1>test test</h1>
-        </>
-    )
-=======
 
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Outlet } from 'react-router';
@@ -24,5 +15,4 @@ return (
         <Outlet />
     </>
 )
->>>>>>> 0bf1ad9c8bd275b6da7837d1c90e6074a4b37e9a
 }
