67aa5363a3ddf7d596082edc13d650896483a588
Ruben <ruben.g.davis@gmail.com>
2025-06-13 16:32:46 +0200

    CustomerInfo modal WIP

--- Frontend/Eindopdracht/src/components/pages/admin/Admin.tsx
+++ Frontend/Eindopdracht/src/components/pages/admin/Admin.tsx
@@ -1,5 +1,5 @@
 import ItemColumns from './ItemColumns'
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import SearchBar from './SearchBar';
 import ItemCard from './ItemCard';
 import { fuelTypeEnum, statusEnum, typeEnum } from '../../../utilities/types';
@@ -7,8 +7,8 @@ import { fuelTypeEnum, statusEnum, typeEnum } from '../../../utilities/types';
 export default function Admin() {
     return (
         <Container style={{ marginTop: '40px', backgroundColor: '#FAF9F9', borderRadius: '6px' }} fluid>
-            <Row>
-                <SearchBar />
+            <Row> 
+                <SearchBar />    
             </Row>
             <Row xs='auto'>
                 <ItemColumns />
