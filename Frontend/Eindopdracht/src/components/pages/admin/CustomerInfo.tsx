8ba7ba82f6a06dd5a437722de39c296834d7c3e2
YDevving <78418986+ydevving@users.noreply.github.com>
2025-06-14 01:31:24 +0200

    Customer searching implemented for admin page

--- Frontend/Eindopdracht/src/components/pages/admin/CustomerInfo.tsx
+++ Frontend/Eindopdracht/src/components/pages/admin/CustomerInfo.tsx
@@ -5,126 +5,134 @@ import type { Transaction } from '../../../utilities/types';
 import { TransactionSchema } from '../../../utilities/types';
 import Session from '../../../utilities/Session';
 
-export default function CustomerInfo({ show, onHide }: { show: boolean, onHide: () => void }){
-    let [userInfo, setUserInfo] = useState({username: '', email: '', city: '', address: '' });
+function checkTransactionDate(startDate: Date, endDate: Date): boolean {
+    const currentDate = new Date();
+    if (currentDate <= endDate && currentDate >= startDate) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function calculateAmountOfDays(startDate: Date, endDate: Date): number {
+    const oneDay = 24 * 60 * 60 * 1000;
+    return (Math.ceil(Math.abs((startDate.valueOf() - endDate.valueOf()) / oneDay)) + 1);
+}
+
+function calculateTotalPrice(startDate: Date, endDate: Date, price: number): number {
+    return (calculateAmountOfDays(startDate, endDate) * price);
+}
+
+export default function CustomerInfo({ show, onHide, username }: { show: boolean, onHide: () => void, username: string }) {
+    
+    const [userInfo, setUserInfo] = useState({ username: '', email: '', city: '', address: '' });
     const [transactions, setTransactions] = useState<Array<Transaction>>([]);
 
+    useEffect(() => {
+        if (show &&
+            username.length > 0
+        ) {
 
-        useEffect(() => {
-            Session.instance.onTokenAvailable((token) => {
-                Session.instance.GET(`/user/admin/info/royce_schut`)
+            const doRequest = async () => {
+                await Session.instance.GET(`/user/admin/info/${username}`)
                     .then((data: Response) => data.json())
-                    .then((json: { username: string, email: string, city: string, address: string }) => {
+                    .then((json) => {
                         setUserInfo({
                             username: json['username'],
                             email: json['email'],
                             city: json['city'],
                             address: json['address']
                         });
-        
-                        console.log(userInfo.username, userInfo.email, userInfo.city, userInfo.address);
                     })
-        })}
-            , []);
+                    .catch((error) => console.error('Error loading JSON', error));
 
-        useEffect(() => {
-            if (transactions.length !== 0)
-                return;
-    
-            Session.instance.onTokenAvailable((token) => {
-                Session.instance.GET('/transaction/admin/royce_schut')
-                    .then((data) => { return data.json(); })
+                await Session.instance.GET(`/transaction/admin/${username}`)
+                    .then((data: Response) => { return data.json(); })
                     .then((transactionData: Array<Transaction>) => {
                         for (const t of transactionData) {
                             try {
                                 t['rentedAt'] = new Date(t.rentedAt);
                                 t['rentedUntil'] = new Date(t.rentedUntil);
-    
+
                                 const data = TransactionSchema.parse(t);
                             }
                             catch (error) {
-                                console.error("Invalid transaction data!", error);
+                                console.error("Invalid transaction data:", error);
                             }
                         }
+
                         transactionData.sort((a, b) => b.rentedAt.valueOf() - a.rentedAt.valueOf());
                         setTransactions(transactionData);
-                        
                     })
                     .catch((error) => console.error('Error loading JSON', error));
-            })
-        }, []);
-    
-        function checkTransactionDate(startDate: Date, endDate: Date): boolean {
-            const currentDate = new Date();
-            if (currentDate <= endDate && currentDate >= startDate) {
-                return true
-            }
-            else {
-                return false
-            }
-        }
-    
-        function calculateAmountOfDays(startDate: Date, endDate: Date): number {
-            const oneDay = 24 * 60 * 60 * 1000;
-            return (Math.ceil(Math.abs((startDate.valueOf() - endDate.valueOf()) / oneDay)) + 1);
-        }
-    
-        function calculateTotalPrice(startDate: Date, endDate: Date, price: number): number {
-            return (calculateAmountOfDays(startDate, endDate) * price);
-        }
-    
-    
-        return (
-            <Container>
-                <Modal show={show} onHide={onHide} size='lg' >
-                    <Modal.Header closeButton>
-                        <Modal.Title>{userInfo.username}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body >
-                        <Row>
-                            <Col><h4 className="mb-1 text-weight-bold">Gegevens</h4></Col>
-                                <p className="my-0">{userInfo.city}</p>
-                                <p className="my-0">{userInfo.address}</p>
-                                <p className="my-0">{userInfo.email} </p>
-                        </Row>
+            };
 
-                        <Row>
-                            <Col>
-                        <Table responsive='lg'>
-                            <thead >
-                                <tr>
-                                    <th>Start datum</th>
-                                    <th>Eind datum</th>
-                                    <th>Type</th>
-                                    <th>Naam</th>
-                                    <th>Prijs</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {transactions.map((transaction) => {
-                                    let itemType = transaction.item.type.toString();
-    
-                                    return (
-                                        <tr key={transaction.id}>
-                                            <td className='align-middle'>{transaction.rentedAt.toLocaleDateString()}</td>
-                                            <td className='align-middle'>{transaction.rentedUntil.toLocaleDateString()}</td>
-                                            <td className='align-middle'>{`${itemType.toUpperCase().charAt(0)}${itemType.slice(1).toLowerCase()}`}</td>
-                                            <td className='align-middle'>{!transaction.item.car ? `${transaction.item.name}` : `${transaction.item.car.brand} ${transaction.item.name}`}</td>
-                                            <td className='align-middle'>{`€${calculateTotalPrice(transaction.rentedAt, transaction.rentedUntil, transaction.item.price)}`}</td>
-                                            <td style={{border: '0'}} className='align-middle'>{checkTransactionDate(transaction.rentedAt, transaction.rentedUntil) ? <Button variant='danger' className='ms-3'>schade melden</Button> : <></>}</td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </Table>
+            doRequest();
+        }
+    }, [username, show]);
+
+    const noTransactionsError = (<><h6 style={{marginTop: '15px',color: 'red'}}>No Transactions found</h6></>);
+
+    return (
+        <Container>
+            <Modal show={show} onHide={onHide} size='lg' >
+                <Modal.Header closeButton>
+                    <Modal.Title>Klant Informatie</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '35px'
+                }}>
+                    <Row>
+                        <Col>
+                            <h4 className="mb-1 text-weight-bold">Gegevens</h4>
                         </Col>
-                        </Row>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={onHide}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
-            </Container>
-        )
+                        <p className="my-0">{userInfo.username}</p>
+                        <p className="my-0">{userInfo.city}</p>
+                        <p className="my-0">{userInfo.address}</p>
+                        <p className="my-0">{userInfo.email} </p>
+                    </Row>
+
+                    <Row>
+                        <h4>Transactions</h4>
+                        <Col>
+                            <Table responsive='lg'>
+                                <thead >
+                                    <tr>
+                                        <th>Start datum</th>
+                                        <th>Eind datum</th>
+                                        <th>Type</th>
+                                        <th>Naam</th>
+                                        <th>Prijs</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {(transactions.length > 0) ? transactions.map((transaction) => {
+                                        let itemType = transaction.item.type.toString();
+
+                                        return (
+                                            <tr key={transaction.id}>
+                                                <td className='align-middle'>{transaction.rentedAt.toLocaleDateString()}</td>
+                                                <td className='align-middle'>{transaction.rentedUntil.toLocaleDateString()}</td>
+                                                <td className='align-middle'>{`${itemType.toUpperCase().charAt(0)}${itemType.slice(1).toLowerCase()}`}</td>
+                                                <td className='align-middle'>{!transaction.item.car ? `${transaction.item.name}` : `${transaction.item.car.brand} ${transaction.item.name}`}</td>
+                                                <td className='align-middle'>{`€${calculateTotalPrice(transaction.rentedAt, transaction.rentedUntil, transaction.item.price)}`}</td>
+                                                <td style={{ border: '0' }} className='align-middle'>{checkTransactionDate(transaction.rentedAt, transaction.rentedUntil) ? <Button variant='danger' className='ms-3'>schade melden</Button> : <></>}</td>
+                                            </tr>
+                                        )
+                                    }) : noTransactionsError}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </Container>
+    )
 
 }
--- Frontend/Eindopdracht/src/components/pages/admin/SearchBar.tsx
+++ Frontend/Eindopdracht/src/components/pages/admin/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Container, Row, Col, Form,Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { FaSearch } from 'react-icons/fa';
-import {useState} from 'react'
+import { useState } from 'react'
 import CustomerInfo from './CustomerInfo';
 
 function searchUser(username: string) {
@@ -14,22 +14,27 @@ function searchUser(username: string) {
 
 export default function SearchBar() {
     const [showCustomerModal, setShowCustomerModal] = useState<boolean>(false)
-    //const [searchedUser, setsearchedUser] = useState("")
-        
-        const handleClose = () => setShowCustomerModal(false);
-        const handleShow = () => setShowCustomerModal(true);
-        
+    const [searchText, setSearchText] = useState<string>(''); 
+
+    const handleClose = () => setShowCustomerModal(false);
+    const handleShow = () => setShowCustomerModal(true);
+
     return (
         <Container>
             <Row className="justify-content-center">
                 <Col md={6}>
-                    <div className="position-relative">
+                    <div className="position-relative" style={{ marginLeft: '15%' }}>
                         <Form.Control
                             type="text"
-                            placeholder="Search user's info..."
+                            placeholder="Zoek klant informatie op..."
                             className="search-input"
                             style={{ paddingRight: '2.5rem' }}
                             onKeyDown={() => { }}
+                            onChange={(event) => {
+                                if (event?.target) {
+                                    setSearchText(event.target.value);
+                                }
+                            }}
                         />
                         <FaSearch
                             style={{
@@ -38,21 +43,21 @@ export default function SearchBar() {
                                 top: '50%',
                                 transform: 'translateY(-50%)',
                                 color: '#6c757d',
-                                pointerEvents: 'none',
+                                pointerEvents: 'none'
                             }}
                         />
                     </div>
                 </Col>
-                <Col md={{span: 2}}>
+                <Col md={{ span: 2 }}>
                     <Button onClick={handleShow} style={{
-                            backgroundColor: '#3D4E6D',
-                            color: '#FFFFFF',
-                            border: 'none',
-                            fontSize: '0.9rem',
-                            }}>Zoek Klant</Button>
-                </Col>    
+                        backgroundColor: '#3D4E6D',
+                        color: '#FFFFFF',
+                        border: 'none',
+                        fontSize: '0.9rem',
+                    }}>Zoek Klant</Button>
+                </Col>
             </Row>
-            <CustomerInfo show={showCustomerModal} onHide={handleClose}></CustomerInfo>
+            <CustomerInfo show={showCustomerModal} onHide={handleClose} username={searchText} ></CustomerInfo>
         </Container>
     );
 };
